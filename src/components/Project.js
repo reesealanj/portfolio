54cde4fc3c490291ee57b1480fd7febb42e13e8d
Reese Jones <reesealanj@gwu.edu>
2020-12-12 13:14:43 -0500

    Implemented single post view

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,5 +1,86 @@
-import React from "react"
+import React, { useEffect, useState } from "react";
+import sanityClient from "../client.js";
 
 export default function Project() {
-    return <h1>Project Page</h1>
+    const [projectData, setProjectData] = useState(null);
+
+    useEffect(() => {
+        sanityClient
+            .fetch(
+                `*[_type == "project"]{
+            title,
+            date,
+            description,
+            projectType,
+            link,
+            github,
+            tags,
+        }`
+            )
+            .then((data) => setProjectData(data))
+            .catch(console.error);
+    }, []);
+
+    return (
+        <main className="bg-green-100 min-h-screen p-12">
+            <section className="container mx-auto">
+                <h1 className="text-5xl flex justify-center fancy">
+                    My Projects
+                </h1>
+                <h2 className="text-lg  flex justify-center mb-12">
+                    These are my projects and previous work
+                </h2>
+                <section className="grid grid-cols-2 gap-8">
+                    {projectData &&
+                        projectData.map((project, index) => (
+                            <article className="relative rounded-lg shadow-xl bg-white p-16">
+                                <h3 className="text-gray-800 text-3xl font-bold mb-2 hover:text-red-700">
+                                    <a
+                                        href={project.link}
+                                        alt={project.title}
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                    >
+                                        {project.title}
+                                    </a>
+                                </h3>
+                                <div className="text-gray-500 text-xs space-x-4">
+                                    <span>
+                                        <strong className="font-bold">
+                                            Finished on:
+                                        </strong>{" "}
+                                        {new Date(
+                                            project.date
+                                        ).toLocaleDateString()}
+                                    </span>
+                                    <span>
+                                        <strong className="font-bold">
+                                            Type:{" "}
+                                        </strong>{" "}
+                                        {project.projectType}
+                                    </span>
+                                    <p className="my-6 text-lg text-gray-700 leading-relaxed">
+                                        {project.description}
+                                    </p>
+                                    <a
+                                        href={project.link}
+                                        rel="noopener noreferrer"
+                                        target="_blank"
+                                        className="text-red-500 font-bold text-l hover:underline hover:text-red-400"
+                                    >
+                                        View the Project{" "}
+                                        <span
+                                            role="img"
+                                            aria-label="right pointer"
+                                        >
+                                            👉
+                                        </span>
+                                    </a>
+                                </div>
+                            </article>
+                        ))}
+                </section>
+            </section>
+        </main>
+    );
 }
